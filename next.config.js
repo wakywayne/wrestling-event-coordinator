ff38b6a3ecd9c1d465936db9630e37399eb75dce
wakywayne <wakywayne80@gmail.com>
2022-10-21 11:53:11 -0400

    thsi is a good place to start a template with buncle analyzer

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+if (process.env.ANALYZE === 'true') {
+  module.exports = withBundleAnalyzer()
+} else {
+  module.exports = nextConfig
+}
+
